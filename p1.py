c4210db9ef9121ba4f32745c8025ea150fb24642
Pankaj <pankaj.pv70@gmail.com>
2020-02-13 22:49:17 +0530

    'a'

---
+++ p1.py
@@ -0,0 +1,32 @@
+# BLL code start here
+StudentList=[]
+class Student():
+    def __init__(self):
+        self.id=0
+        self.name=''
+    def AddStudent(self):
+        StudentList.append(self)
+
+    def showAll(self):
+        return StudentList
+
+
+
+# BLL code end here
+# PL code start here (Crud )
+while (True):
+    print("1.Add Student\n2.Search Student\n3.Update Student\n4.Delete Student\nExit press 0")
+    ch=input('Enter your choice')
+    if ch=='1':
+        stdt=Student()
+        stdt.id=int(input('Enter your id:  '))
+        stdt.name=input('Enter your Name:  ')
+        stdt.AddStudent()
+        print('Student Added successfully')
+    if ch=='5':
+        stdt = Student()
+        s =stdt.showAll()
+        for stu in s:
+            print(stu.id, stu.name)
+
+# PL code end here
